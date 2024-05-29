ce188746efa68aee9ae6fdf63899c5a70fa8f179
kaiki uezawa <kaiki0202k@gmail.com>
2024-05-29 10:35:30 +0900

    test

--- react-firebase-auth/src/List.js
+++ react-firebase-auth/src/List.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 import { db } from "./firebase";
 import { collection, getDocs, query, onSnapshot, querySnapshot } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function Lists() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {}, []);
+function List() {
+  const { id } = useParams();
+  console.log(id);
 
   return <></>;
 }
 
-export default Lists;
+export default List;
